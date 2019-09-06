61d4d2076a5c0ba0762cae2a011dd192556cc316
Alexander <efimov@fai.utb.cz>
2019-09-06 15:41:11 +0200

    Create SubSolution.java

---
+++ com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    SubSolution(double a, String s) {
+        super(a);
+    }
+
+    protected SubSolution(double b) {
+        super(b);
+    }
+
+    public SubSolution(Double c) {
+        super(c);
+    }
+
+
+}
